cd5df88d54cd66571c525b9648f097a502dd2b9f
flipped <greedkiss@163.com>
2020-08-04 08:55:58 +0800

    :tada:中间节点的分裂导致树的增高未实现，anyway, 开始看文献了

--- db.c
+++ db.c
@@ -591,7 +591,7 @@ void* leaf_node_value(void* node, uint32_t cell_num){
 }
 
 //将Row中的信息拷贝到缓冲区pager中
-void serilaize_row(Row* resource, void* destination){
+void serialize_row(Row* resource, void* destination){
   memcpy(destination+ ID_OFFSET, &resource->id, ID_SIZE);
   memcpy(destination + USERNAME_OFFSET, &resource->username, USERNAME_SIZE);
   memcpy(destination + EMAIL_OFFSET, &resource->email, EMAIL_SIZE);
@@ -623,9 +623,46 @@ void create_new_root(Table* table, uint32_t right_child_page_num){
   *node_parent(right_child) = table->root_page_num;
 }
 
+//替换old_key为new_key
 void update_internal_node_key(void* node, uint32_t old_key, uint32_t new_key){
   uint32_t old_child_index = internal_node_find_child(node, old_key);
   *internal_node_key(node, old_child_index) = new_key;
+}
+
+void internal_node_insert(Table* table, uint32_t parent_page_num , uint32_t child_page_num){
+  void* parent = get_page(table->pager, parent_page_num);
+  void* child = get_page(table->pager, child_page_num);
+  uint32_t child_max_key = get_node_max_key(child);
+  uint32_t index = internal_node_find_child(parent, child_max_key);
+
+  // 之前Parent节点key数量
+  uint32_t original_num_keys = *internal_node_num_keys(parent);
+  *internal_node_num_keys(parent) = original_num_keys + 1;
+
+  if (original_num_keys >= INTERNAL_NODE_MAX_CELLS) {
+    printf("Need to implement splitting internal node\n");
+    exit(EXIT_FAILURE);
+  }
+
+  uint32_t right_child_page_num = *internal_node_right_child(parent);
+  void* right_child = get_page(table->pager, right_child_page_num);
+
+  if (child_max_key > get_node_max_key(right_child)) {
+    /* Replace right child */
+    *internal_node_child(parent, original_num_keys) = right_child_page_num;
+    *internal_node_key(parent, original_num_keys) =
+        get_node_max_key(right_child);
+    *internal_node_right_child(parent) = child_page_num;
+  } else {
+    /* Make room for the new cell */
+    for (uint32_t i = original_num_keys; i > index; i--) {
+      void* destination = internal_node_cell(parent, i);
+      void* source = internal_node_cell(parent, i - 1);
+      memcpy(destination, source, INTERNAL_NODE_CELL_SIZE);
+    }
+    *internal_node_child(parent, index) = child_page_num;
+    *internal_node_key(parent, index) = child_max_key;
+  }
 }
 
 //b数节点分裂
@@ -646,6 +683,7 @@ void leaf_node_split_and_insert(Cursor* cursor, uint32_t key, Row* value){
     }else{
       destination_node = old_node;
     }
+    //把old_node均分为两个，大的放在new_nonde
     uint32_t index_within_node = i % LEAF_NODE_LEFT_SPLIT_COUNT;
     void* destination = leaf_node_cell(destination_node, index_within_node);
 
@@ -671,17 +709,10 @@ void leaf_node_split_and_insert(Cursor* cursor, uint32_t key, Row* value){
       void * parent = get_page(cursor->table->pager, parent_page_num);
 
       update_internal_node_key(parent, old_max, new_max); 
+      internal_node_insert(cursor->table, parent_page_num, new_page_num);
+      return;
     }
-
-
-
-
-
   }
-
-
-
-
 }
 
 void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value){
@@ -689,9 +720,20 @@ void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value){
 
   uint32_t num_cells = *leaf_node_num_cells(node);
   //分裂
-  if(num_cells > LEAF_NODE_MAX_CELLS){
+  if(num_cells >= LEAF_NODE_MAX_CELLS){
     leaf_node_split_and_insert(cursor, key, value);
   }
+  if (cursor->cell_num < num_cells) {
+    // Make room for new cell
+    for (uint32_t i = num_cells; i > cursor->cell_num; i--) {
+      memcpy(leaf_node_cell(node, i), leaf_node_cell(node, i - 1),
+             LEAF_NODE_CELL_SIZE);
+    }
+  }
+
+  *(leaf_node_num_cells(node)) += 1;
+  *(leaf_node_key(node, cursor->cell_num)) = key;
+  serialize_row(value, leaf_node_value(node, cursor->cell_num));
 }
 
 ExecuteResult execute_insert(Statement* statement, Table* table){
@@ -713,10 +755,33 @@ ExecuteResult execute_insert(Statement* statement, Table* table){
   leaf_node_insert(cursor, row_to_insert->id, row_to_insert);
 }
 
+void deserialize_row(void* source, Row* destination) {
+  memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+  memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+  memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+}
+
+ExecuteResult execute_select(Statement* statement, Table* table) {
+  Cursor* cursor = table_start(table);
+
+  Row row;
+  while (!(cursor->end_of_table)) {
+    deserialize_row(cursor_value(cursor), &row);
+    print_row(&row);
+    cursor_advance(cursor);
+  }
+
+  free(cursor);
+
+  return EXECUTE_SUCCESS;
+}
+
 ExecuteResult execute_statement(Statement* statement , Table* table){
   switch(statement->type) {
     case(STATEMENT_INSERT):
       return execute_insert(statement, table);
+    case(STATEMENT_SELECT):
+      return execute_select(statement, table);
   }
 }
 
@@ -770,8 +835,5 @@ int main(int argc, char * argv[]){
         printf("error: duplicate key.\n");
         break;
     }
-
   }
-
-
 }
